00c1b9f5284cc8d00f7204715ff3616bd0569b0d
Daniel Irimia <91692405+danielirimia60@users.noreply.github.com>
2022-01-17 23:32:47 +0000

    Update nav.js

--- Resources/Components/nav.js
+++ Resources/Components/nav.js
@@ -1,5 +1,6 @@
 const nav = document.querySelector(".main-nav-bar");
 const navToggle = document.querySelector(".mobile-nav-toggle");
+const navBackground = document.querySelector(".nav-wrap");
 
 navToggle.addEventListener('click', () => {
 
@@ -7,8 +8,10 @@ navToggle.addEventListener('click', () => {
     if (visiblity === "false") {
         nav.setAttribute("data-visible", true);
         navToggle.setAttribute("aria-expanded", true);
+        navBackground.style.background = "hsla(0, 0%, 100%, 1)";
     } else {
         nav.setAttribute("data-visible", false);
         navToggle.setAttribute("aria-expanded", false);
+        navBackground.style.background = "hsla(187, 2%, 98%, 0.5)";
     }
 })
